774b4a389d78e4487a9f507296916edb115b7346
Shaina C <scailles23@gmail.com>
2020-01-22 23:39:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,39 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song= self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song=self.new
+    song.name= name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song= self.new
+    song.name= name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    result= self.find_by_name(name)
+   if result
+     result
+     else
+       self.create_by_name(name)
+     end
+ end
+   
+    
+  
 end
